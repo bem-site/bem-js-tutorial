d837dd5e6926ee379e5affd040b90741d3c3a052
Varvara Stepanova <toivonen@toivonen.ru>
2013-07-22 18:15:50 +0400

    Codestyle

--- desktop.bundles/001-simple-block/blocks/my-block/my-block.js
+++ desktop.bundles/001-simple-block/blocks/my-block/my-block.js
@@ -1,27 +1,22 @@
 modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
 
-    DOM.decl('my-block', {
-
-        onSetMod: {
-
-            'js' : {
-
-                'inited' : function() {
-                    var text = [
-                        'This is a block represented in HTML like <br/>',
-                        $('<div/>').text(this.domElem[0].outerHTML).html(),
-                        '<br/>',
-                        ' and equiped with JavaScript functionality. ',
-                        'The text you are reading now was appended dynamically.'
-                    ];
-                    this.domElem.html(text);
-                }
+DOM.decl('my-block', {
+    onSetMod: {
+        'js' : {
+            'inited' : function() {
+                var text = [
+                    'This is a block represented in HTML like <br/>',
+                    $('<div/>').text(this.domElem[0].outerHTML).html(),
+                    '<br/>',
+                    ' and equiped with JavaScript functionality. ',
+                    'The text you are reading now was appended dynamically.'
+                ];
+                this.domElem.html(text);
             }
-
         }
+    }
+});
 
-    });
-
-    provide(DOM);
+provide(DOM);
 
 });
