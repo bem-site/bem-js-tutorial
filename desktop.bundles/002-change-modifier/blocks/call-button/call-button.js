6c4e5f6dea065a2c33a3c3588b6b0310a1c973f8
Varvara Stepanova <toivonen@toivonen.ru>
2013-07-23 19:13:35 +0400

    Nice ico on the call-button

--- desktop.bundles/002-change-modifier/blocks/call-button/call-button.js
+++ desktop.bundles/002-change-modifier/blocks/call-button/call-button.js
@@ -1,12 +1,17 @@
 modules.define('i-bem__dom', function(provide, DOM) {
 
-DOM.decl('square', {
+DOM.decl('call-button', {
     onSetMod: {
         'js' : {
             'inited' : function() {
                 this.bindTo('click', function() {
-                    this.setMod('theme', 'new-year');
+                    this.setMod('status', 'calling');
                 });
+            }
+        },
+        'status' : {
+            'calling' : function() {
+                this.elem('link').text('Calling...');
             }
         }
     }
