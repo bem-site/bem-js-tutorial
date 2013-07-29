077f88e876fdf276a8dd609ddb55735b8d9910cd
Varvara Stepanova <toivonen@toivonen.ru>
2013-07-29 17:56:34 +0400

    A dummy page

---
+++ desktop.bundles/004-toggle-mod/004-toggle-mod.bemjson.js
@@ -0,0 +1,13 @@
+({
+    block: 'page',
+    title: 'Toggling a modifiers on a block and an element',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_004-toggle-mod.css', ie: false },
+        { elem: 'css', url: '_004-toggle-mod', ie: true },
+        { elem: 'meta', attrs: { name: 'description', content: '' }}
+    ],
+    content:[
+        { elem: 'js', url: '_004-toggle-mod.js' }
+    ]
+})
