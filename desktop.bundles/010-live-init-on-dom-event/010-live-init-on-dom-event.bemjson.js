9f6e5f992da2b33708b5abd1c70c258ed2af7348
Varvara Stepanova <toivonen@toivonen.ru>
2013-08-20 17:59:12 +0400

    Buttons of different color and shape

--- desktop.bundles/010-live-init-on-dom-event/010-live-init-on-dom-event.bemjson.js
+++ desktop.bundles/010-live-init-on-dom-event/010-live-init-on-dom-event.bemjson.js
@@ -1,16 +1,16 @@
 (function() {
 
     var buttons = [],
-        colors = ['orange', 'blue', 'green', 'grey'],
+        colors = ['orange', 'blue', 'green', 'grey', 'pink'],
         shape = ['round', 'oval', 'brackets', 'skew', 'back', 'knife', 'shield', 'drop', 'morph'];
     for(var i=0;i<100;i++) {
         buttons.push({
             block: 'button',
             mods: {
-                color: colors[Math.floor((Math.random()*(colors.length-1)))],
-                shape: shape[Math.floor((Math.random()*(shape.length-1)))]
+                color: colors[Math.round((Math.random()*(colors.length-1)))],
+                shape: shape[Math.round((Math.random()*(shape.length-1)))]
             },
-            content: 'Button #' + i
+            content: 'Button #' + (i+1)
         });
     }
 
@@ -24,6 +24,7 @@
             { elem: 'meta', attrs: { name: 'description', content: '' }},
         ],
         content:[
+            'Famous BonBon CSS3 buttons by Simurai (http://archive.simurai.com/lab/buttons/) illustrate lazy initialization in BEM',
             buttons,
             { elem: 'js', url: '_010-live-init-on-dom-event.js' }
         ]
