d1aa9d53dad5a89585d65f31407c03e242395ce2
Varvara Stepanova <toivonen@toivonen.ru>
2013-08-20 17:41:11 +0400

    100 buttons of different shape and color

--- desktop.bundles/010-live-init-on-dom-event/010-live-init-on-dom-event.bemjson.js
+++ desktop.bundles/010-live-init-on-dom-event/010-live-init-on-dom-event.bemjson.js
@@ -1,13 +1,22 @@
 (function() {
 
-    var buttons = [];
+    var buttons = [],
+        colors = ['orange', 'blue', 'green', 'grey'],
+        shape = ['round', 'oval', 'brackets', 'skew', 'back', 'knife', 'shield', 'drop', 'morph'];
     for(var i=0;i<100;i++) {
-        buttons.push({ block: 'button' });
+        buttons.push({
+            block: 'button',
+            mods: {
+                color: colors[Math.floor((Math.random()*(colors.length-1)))],
+                shape: shape[Math.floor((Math.random()*(shape.length-1)))]
+            },
+            content: 'Button #' + i
+        });
     }
 
     return {
         block: 'page',
-        title: 'Removing a modifier from an element',
+        title: 'Live (lazy) initialization on DOM event',
         favicon: '/favicon.ico',
         head: [
             { elem: 'css', url: '_010-live-init-on-dom-event.css', ie: false },
