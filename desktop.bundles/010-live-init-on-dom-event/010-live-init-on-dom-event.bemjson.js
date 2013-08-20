7ba9b88e47c81aebe470d27dad0131cdf5da7d94
Varvara Stepanova <toivonen@toivonen.ru>
2013-08-20 18:34:34 +0400

    Nice layout

--- desktop.bundles/010-live-init-on-dom-event/010-live-init-on-dom-event.bemjson.js
+++ desktop.bundles/010-live-init-on-dom-event/010-live-init-on-dom-event.bemjson.js
@@ -4,14 +4,14 @@
         colors = ['orange', 'blue', 'green', 'grey', 'pink'],
         shape = ['round', 'oval', 'brackets', 'skew', 'back', 'knife', 'shield', 'drop', 'morph'];
     for(var i=0;i<100;i++) {
-        buttons.push({
+        buttons.push([{
             block: 'button',
             mods: {
                 color: colors[Math.round((Math.random()*(colors.length-1)))],
                 shape: shape[Math.round((Math.random()*(shape.length-1)))]
             },
             content: 'Button #' + (i+1)
-        });
+        }, '<br/>']);
     }
 
     return {
@@ -24,7 +24,10 @@
             { elem: 'meta', attrs: { name: 'description', content: '' }},
         ],
         content:[
-            'Famous BonBon CSS3 buttons by Simurai (http://archive.simurai.com/lab/buttons/) illustrate lazy initialization in BEM',
+            {
+                block: 'description',
+                content: 'Famous BonBon CSS3 buttons by Simurai (http://archive.simurai.com/lab/buttons/) illustrate lazy initialization in BEM'
+            },
             buttons,
             { elem: 'js', url: '_010-live-init-on-dom-event.js' }
         ]
