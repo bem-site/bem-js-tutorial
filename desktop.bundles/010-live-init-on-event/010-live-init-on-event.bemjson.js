07d6541d79e61aab564bf8ccb951144742c4fc21
Varvara Stepanova <toivonen@toivonen.ru>
2013-08-22 12:29:29 +0400

    One more funny story in Dutch

--- desktop.bundles/010-live-init-on-event/010-live-init-on-event.bemjson.js
+++ desktop.bundles/010-live-init-on-event/010-live-init-on-event.bemjson.js
@@ -24,7 +24,9 @@
                         'Нидерландский шутя',
                         '</a>, ',
                         'which is an adopted version of some Dutch jokes for Russian-speaking ',
-                        'learners.'
+                        'learners.<br/>',
+                        'Read the text trying to understand the meaning. If you do not know ',
+                        'what it means, click to see the translation.'
                     ]
                 },
                 {
@@ -190,6 +192,221 @@
                             prompt: 'ну, я просто побежал: «пошел бежать» домой'
                         },
                         '”.'
+                    ]
+                },
+                {
+                    elem: 'h2',
+                    mix: {
+                        block: 'translate',
+                        js: { prompt: 'лабрадор' }
+                    },
+                    content: [
+                        'Labrador',
+                        { block: 'translate', elem: 'prompt' }
+                    ]
+                },
+                {
+                    elem: 'p',
+                    content: [
+                        {
+                            block: 'translate',
+                            content: 'Een jager leest',
+                            prompt: 'один охотник читает'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'eenadvertentie in de krant',
+                            prompt: 'объявление в газете'
+                        },
+                        ': ',
+                        {
+                            block: 'translate',
+                            content: 'unieke jachthond te koop',
+                            prompt: 'продается уникальная охотничья собака'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: 'Hij besluit',
+                            prompt: 'он решает'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'te gaan kijken',
+                            prompt: 'поехать посмотреть'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: 'De verkoper neemt hem mee naar het hok',
+                            prompt: 'продавец ведет: «берет» его с собой к будке'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: 'De jager kijkt en zegt',
+                            prompt: 'охотник смотрит и говорит'
+                        },
+                        ': ',
+                        {
+                            block: 'translate',
+                            content: '“Maar dat is gewoon een labrador',
+                            prompt: 'но это просто лабрадор'
+                        },
+                        '”. '
+                    ]
+                },
+                {
+                    elem: 'p',
+                    content: [
+                        {
+                            block: 'translate',
+                            content: '”Ja”, zegt de verkoper',
+                            prompt: 'да, говорит продавец'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: '“maar deze hond is echt uniek',
+                            prompt: 'но эта собака по-настоящему уникальна'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: 'We zullen wel eens even een stukje gaan jagen',
+                            prompt: 'мы пойдем, пожалуй, как раз немного поохотимся'
+                        },
+                        '.”'
+                    ]
+                },
+                {
+                    elem: 'p',
+                    content: [
+                        {
+                            block: 'translate',
+                            content: 'De verkoper haalt zijn geweer',
+                            prompt: 'продавец достает свое ружье'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'en de twee lopen met de hond naar het meer toe',
+                            prompt: 'и двое идут с собакой к озеру'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: 'De verkoper knalt',
+                            prompt: 'продавец ухлопывает'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'een eend uit de lucht',
+                            prompt: 'утку с лету'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'en de eend valt in het water',
+                            prompt: 'и утка падает в воду'
+                        },
+                        '. ',
+                        {
+                            block: 'translate',
+                            content: 'Dehond loopt over het water',
+                            prompt: 'собака бежит по воде'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'naar de eend toe',
+                            prompt: 'к утке'
+                        },
+                        ', ',
+                        {
+                            block: 'translate',
+                            content: 'neemt hem in zijn bek',
+                            prompt: 'берет ее: «его» в зубы'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'en brengt hem over het water lopend',
+                            prompt: 'и несет ее, бежа по воде'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'mee terug naar de twee mannen',
+                            prompt: 'назад к двум мужчинам'
+                        },
+                        '.'
+                    ]
+                },
+                {
+                    elem: 'p',
+                    content: [
+                        {
+                            block: 'translate',
+                            content: '”Dat is inderdaad bijzonder',
+                            prompt: 'это в самом деле особенно'
+                        },
+                        ',” ',
+                        {
+                            block: 'translate',
+                            content: 'zegt de jager',
+                            prompt: 'говорит охотник'
+                        },
+                        ', ',
+                        {
+                            block: 'translate',
+                            content: '“wat moet die hond kosten',
+                            prompt: 'ну и сколько же стоит: «должна стоить» та собака'
+                        },
+                        '?”'
+                    ]
+                },
+                {
+                    elem: 'p',
+                    content: [
+                        {
+                            block: 'translate',
+                            content: '”Ja”, zegt de verkoper,“hij is natuurlijk niet goedkoop',
+                            prompt: 'да, говорит продавец, она конечно не дешевая'
+                        },
+                        ': ',
+                        {
+                            block: 'translate',
+                            content: '5000 gulden',
+                            prompt: '5000 гульденов'
+                        },
+                        '”.'
+                    ]
+                },
+                {
+                    elem: 'p',
+                    content: [
+                        {
+                            block: 'translate',
+                            content: '”Wat',
+                            prompt: 'что?'
+                        },
+                        ',” ',
+                        {
+                            block: 'translate',
+                            content: 'zegt de jager, “5000 gulden voor een hond',
+                            prompt: '5000 гульденов за собаку'
+                        },
+                        ' ',
+                        {
+                            block: 'translate',
+                            content: 'die niet kan zwemmen',
+                            prompt: 'которая не умеет плавать'
+                        },
+                        '?”'
                     ]
                 }
             ]
