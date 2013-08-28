9aba9bb7ae544cbf6a2114dcdbdee94e0952cc9a
Varvara Stepanova <toivonen@toivonen.ru>
2013-08-28 13:40:03 +0400

    An empty page

---
+++ desktop.bundles/006-before-set-mod/006-before-set-mod.bemjson.js
@@ -0,0 +1,14 @@
+({
+    block: 'page',
+    title: 'beforeSetMod helper',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_006-before-set-mod.css', ie: false },
+        { elem: 'css', url: '_006-before-set-mod', ie: true },
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        '<link  href="http://fonts.googleapis.com/css?family=Reenie+Beanie:regular" rel="stylesheet" type="text/css">'
+    ],
+    content:[
+        { elem: 'js', url: '_006-before-set-mod.js' }
+    ]
+})
