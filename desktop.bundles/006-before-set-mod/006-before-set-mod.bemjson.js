2acf02069a6e72e73b5a23edb9dce3da147a2488
Varvara Stepanova <toivonen@toivonen.ru>
2013-08-29 13:19:21 +0400

    Data for accordeon menu

--- desktop.bundles/006-before-set-mod/006-before-set-mod.bemjson.js
+++ desktop.bundles/006-before-set-mod/006-before-set-mod.bemjson.js
@@ -10,20 +10,86 @@
     ],
     content:[
         {
-            block: 'button',
-            mods: { theme: 'yellow' },
-            text: 'Add to cart',
-            price: '$9,99'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'yellow', disabled: true },
-            text: 'Bring it home',
-            price: '$10,99'
-        },
-        {
-            block: 'experiment',
-            content: 'Try to focus button by button'
+            block: 'accordeon-menu',
+            items: [
+                {
+                    title: 'My files',
+                    items: [
+                        {
+                            title: 'Dropbox',
+                            url: '#',
+                            number: 42
+                        },
+                        {
+                            title: 'Skydrive',
+                            url: '#',
+                            number: 87
+                        },
+                        {
+                            title: 'FTP Server',
+                            url: '#',
+                            number: 366
+                        }
+                    ]
+                },
+                {
+                    title: 'Mail',
+                    items: [
+                        {
+                            title: 'Hotmail',
+                            url: '#',
+                            number: 9
+                        },
+                        {
+                            title: 'Yahoo',
+                            url: '#',
+                            number: '14'
+                        },
+                        {
+                            title: 'Gmail',
+                            url: '#',
+                            number: 3
+                        }
+                    ]
+                },
+                {
+                    title: 'Cloud',
+                    items: [
+                        {
+                            title: 'Connect',
+                            url: '#',
+                            number: 12
+                        },
+                        {
+                            title: 'Profiles',
+                            url: '#',
+                            number: 19
+                        },
+                        {
+                            title: 'Options',
+                            url: '#',
+                            number: 27
+                        }
+                    ]
+                },
+                {
+                    title: 'Sign Out',
+                    items: [
+                        {
+                            title: 'Log Out',
+                            url: '#'
+                        },
+                        {
+                            title: 'Delete Account',
+                            url: '#'
+                        },
+                        {
+                            title: 'Freeze Account',
+                            url: '#'
+                        }
+                    ]
+                }
+            ]
         },
         { elem: 'js', url: '_006-before-set-mod.js' }
     ]
