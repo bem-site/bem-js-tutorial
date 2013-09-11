93d4f125442d7b4d569c41a30a6161ae67dd420b
Varvara Stepanova <toivonen@toivonen.ru>
2013-09-11 14:42:16 +0200

    rying to use beforeElemSetMod

--- desktop.bundles/006-before-set-mod/blocks/accordion-menu/accordion-menu.js
+++ desktop.bundles/006-before-set-mod/blocks/accordion-menu/accordion-menu.js
@@ -1,13 +1,21 @@
-modules.define('i-bem__dom', function(provide, DOM) {
+modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
 
 
 DOM.decl('accordion-menu', {
-    beforeSetMod: {
+    beforeElemSetMod: {
+        'item' : {
+            'current' : function() {
+                console.log(333);
+            }
+        }
     },
     onSetMod: {
         'js' : {
             'inited' : function() {
-                console.log('xxx');
+                this._current = this.findElem('item', 'current', true);
+                this.bindTo('item', 'click', function(e) {
+                    this.setMod($(e.currentTarget), 'current', true);
+                });
             }
         }
     }
