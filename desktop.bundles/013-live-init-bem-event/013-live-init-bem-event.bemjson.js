783a59a76c6493e80fd9c6eb63b8f0275683be56
Varvara Stepanova <toivonen@toivonen.ru>
2013-09-23 21:32:01 +0200

    A dummy page for an example with BEM events

---
+++ desktop.bundles/013-live-init-bem-event/013-live-init-bem-event.bemjson.js
@@ -0,0 +1,13 @@
+({
+    block: 'page',
+    title: 'Live (lazy) initialization on BEM events',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_013-live-init-bem-event.css', ie: false },
+        { elem: 'css', url: '_013-live-init-bem-event', ie: true },
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+    ],
+    content:[
+        { elem: 'js', url: '_013-live-init-bem-event.js' }
+    ]
+})
