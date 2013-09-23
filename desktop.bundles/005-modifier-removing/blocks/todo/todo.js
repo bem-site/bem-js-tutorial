65eda85983883d65cb95949127a3878b258cb809
Varvara Stepanova <toivonen@toivonen.ru>
2013-09-23 21:16:58 +0200

    Fast fix to 005 example

--- desktop.bundles/005-modifier-removing/blocks/todo/todo.js
+++ desktop.bundles/005-modifier-removing/blocks/todo/todo.js
@@ -1,11 +1,11 @@
-modules.define('i-bem__dom', function(provide, DOM) {
+modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
 
 DOM.decl('todo', {
     onSetMod: {
         'js' : {
             'inited' : function() {
                 this.bindTo(this.elem('task'), 'click', function(e) {
-                    this.delMod(e.domElem, 'visible');
+                    this.delMod($(e.currentTarget), 'visible');
                 });
             }
         }
