ce77f4d282df6dedf2f4cf432a55da52e49821a5
Varvara Stepanova <toivonen@toivonen.ru>
2013-09-25 00:03:49 +0200

    Checkbox usage

--- components.bundles/013-live-init-bem-event/013-live-init-bem-event.bemjson.js
+++ components.bundles/013-live-init-bem-event/013-live-init-bem-event.bemjson.js
@@ -9,11 +9,18 @@
     ],
     content:[
         {
+            block : 'checkbox',
+            name : 'r1',
+            mods : { checked : true },
+            val : 2,
+            text : 'label2'
+        },
+        /*{
             block: 'input',
             name: 'password',
             mods: { size: 's' },
             content: { elem: 'control' }
-        },
+        },*/
         { elem: 'js', url: '_013-live-init-bem-event.js' }
     ]
 })
