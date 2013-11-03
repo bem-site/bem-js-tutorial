ba4186efbbacd5d7677dfa36a20728d9198580a3
Varvara Stepanova <toivonen@toivonen.ru>
2013-11-03 21:44:53 +0100

    Endless rotation for disks

--- components.bundles/013-live-init-bem-event/blocks/dj/dj.js
+++ components.bundles/013-live-init-bem-event/blocks/dj/dj.js
@@ -2,15 +2,30 @@ modules.define('i-bem__dom', function(provide, DOM) {
 
 DOM.decl('dj', {
     onSetMod: {
-        'js' : {
-            'inited' : function() {
-                console.log('xxx');
+        'running' : {
+            'true' : function() {
+                var dj = this;
+                dj.findBlocksInside('player').forEach(function(player) {
+                    var rotation = function (){
+                    player.elem('vinyl').rotate({
+                        angle:0, 
+                        animateTo:360, 
+                        callback: rotation,
+                        easing: function (x,t,b,c,d){        // t: current time, b: begInnIng value, c: change In value, d: duration
+                            return c*(t/d)+b;
+                        }
+                    });
+                    }
+                    rotation();
+                });
             }
         }
     }
 },{
     live: function() {
-        this.liveInitOnBlockInsideEvent('init', 'checkbox');
+        this.liveInitOnBlockInsideEvent('change', 'checkbox', function(data) {
+            this.setMod('running', data.target.hasMod('checked'));
+        });
     }
 });
 
