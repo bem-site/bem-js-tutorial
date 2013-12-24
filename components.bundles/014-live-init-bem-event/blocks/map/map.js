eb2fee48e4147a5d8223b0655ff558f2e61e4c32
Varvara Stepanova <toivonen@toivonen.ru>
2013-12-24 15:11:06 +0100

    Show an address on the map block

--- components.bundles/014-live-init-bem-event/blocks/map/map.js
+++ components.bundles/014-live-init-bem-event/blocks/map/map.js
@@ -5,9 +5,29 @@ DOM.decl('map', {
     onSetMod: {
         'js' : {
             'inited' : function () {
+                var address = this.params['address'];
                 var myMap = new ymaps.Map (this.domElem[0], {
                     center: [55.76, 37.64],
                     zoom: 7
+                });
+                ymaps.geocode(address).then(function(res){
+                var firstGeoObject = res.geoObjects.get(0),
+                    // Координаты геообъекта.
+                    coords = firstGeoObject.geometry.getCoordinates(),
+                    // Область видимости геообъекта.
+                    bounds = firstGeoObject.properties.get('boundedBy');
+
+                    //myMap.geoObjects.add(firstGeoObject);
+                    myMap.setBounds(bounds, {
+                        checkZoomRange: true // проверяем наличие тайлов на данном масштабе.
+                    });
+                    var myPlacemark = new ymaps.Placemark(coords, {
+                        iconContent: address,
+                    }, {
+                        preset: 'twirl#violetStretchyIcon'
+                    });
+
+                    myMap.geoObjects.add(myPlacemark);
                 });
             }
         }
